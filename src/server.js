70b6994643e38cc164b01b80c749afbd7dc88fc4
Mohamed <moha242611@gmail.com>
2025-09-10 20:54:11 +0300

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ import { ENV } from './config/env.js';
 import { db } from './config/db.js';
 import { favoritesTable } from './db/schema.js';
 import { eq, and } from 'drizzle-orm';
+import job from './config/cron.js';
 
 const app = express();
 const PORT = ENV.PORT || 5001;
+
+// Start the cron job only in production
+if (ENV.NODE_ENV === 'production') job.start();
 
 app.use(express.json()); // Middleware to parse JSON request bodies if not exists the body was undefined 
 
